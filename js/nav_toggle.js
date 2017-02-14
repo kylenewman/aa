d26beded14c080d60bd9789dc02ed2f35db68c7d
kylenewman <kndesign@yahoo.com>
2017-02-13 20:34:45 -0600

    nav adjustments, responsive implementation

--- js/nav_toggle.js
+++ js/nav_toggle.js
@@ -1,11 +1,16 @@
 jQuery(document).ready(function() {
  
-      $(document).ready(function(){
         $("#navIcon").click(function(){
             $(".navToggle").animate({
                 width: "toggle"
             });
         });
-    });
+  
+      $("#navIcon").click(function(){
+            $(".navOverlay").animate({
+                width: "toggle"
+            });
+        });
+  
 
 });
